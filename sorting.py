1417996452949a7d18ac553a4b535e404743e17d
Hannah Schafer <schafer.hannah@gmail.com>
2017-05-14 11:04:15 -0700

    finished sorting algorithms

--- sorting.py
+++ sorting.py
@@ -7,7 +7,11 @@ def bubble_sort(lst):
         >>> bubble_sort([3, 5, 7, 2, 4, 1])
         [1, 2, 3, 4, 5, 7]
     """
-    pass
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - 1):
+            if lst[j] > lst[j +1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+    return lst
 
 
 def merge_lists(list1, list2):
@@ -18,8 +22,19 @@ def merge_lists(list1, list2):
     [1, 3, 4, 7, 9, 11]
     """
 
-    pass
+    result_list = []
 
+    while len(list1) > 0 or len(list2) > 0:
+        if list1 == []:
+            result_list.append(list2.pop(0))
+        elif list2 ==[]:
+            result_list.append(list1.pop(0))
+        elif list1[0] < list2[0]:
+            result_list.append(list1.pop(0))
+        else:
+            result_list.append(list2.pop(0))
+
+    return result_list
 
 ##########ADVANCED##########
 def merge_sort(lst):
@@ -35,7 +50,36 @@ def merge_sort(lst):
     >>> merge_sort([6, 2, 3, 9, 0, 1])
     [0, 1, 2, 3, 6, 9]
     """
-    pass
+
+    if len(lst) < 2:
+        return lst
+
+    # find midpoint of list
+    mid = int(len(lst) / 2)
+    lst1 = merge_sort(lst[:mid])
+    lst2 = merge_sort(lst[mid:])
+
+    return make_merge(lst1, lst2)
+
+    
+
+def make_merge(lst1, lst2):
+
+    result_list = []
+
+    while len(lst1) > 0 or len(lst2) > 0:
+        if lst1 == []:
+            result_list.append(lst2.pop(0))
+        elif lst2 == []:
+            result_list.append(lst1.pop(0))
+        elif lst1[0] < lst2[0]:
+            result_list.append(lst1.pop(0))
+        else:
+            result_list.append(lst2.pop(0))
+
+    return result_list
+
+
 
 
 
